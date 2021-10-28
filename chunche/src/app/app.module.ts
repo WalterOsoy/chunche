e5fc35b9c9c241361aefb61a9b67d4f08070fe1f
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2021-10-27 22:45:35 -0600

    estructura base

--- chunche/src/app/app.module.ts
+++ chunche/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScoreComponent } from './score/score.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

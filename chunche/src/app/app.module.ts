54cd9127b10355a922bd50cbc3389ff1869995b2
Walter Orozco <walterof1@hotmail.com>
2021-11-01 20:25:22 -0600

    navbar

--- chunche/src/app/app.module.ts
+++ chunche/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SearchComponent } from './search/search.component';
 import { ScoreComponent } from './score/score.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { ScoreComponent } from './score/score.component';
     LoginComponent,
     SignInComponent,
     SearchComponent,
-    ScoreComponent
+    ScoreComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

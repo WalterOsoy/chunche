15568a5490d755c529c9ee28b96dee893b7ff5a2
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2021-11-02 18:58:32 -0600

    ejecutando archivo python

--- chuncheBack/server.js
+++ chuncheBack/server.js
@@ -35,14 +35,6 @@ db.mongoose
 app.get("/", (req, res) => {
   res.json({ message: "Welcome application." });
 });
-
-require("./app/routes/turorial.routes")(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 app.get('/python', (req, res) => {
 
   var dataToSend;
@@ -61,3 +53,10 @@ app.get('/python', (req, res) => {
   });
 
 })
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});

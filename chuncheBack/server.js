3830b3f08f8ead077aacc35278b968e10833f729
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2021-11-02 18:49:50 -0600

    ejecutando archivo python

--- chuncheBack/server.js
+++ chuncheBack/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {spawn} = require('child_process');
 // const bodyParser = require("body-parser"); /* deprecated */
 const cors = require("cors");
 
@@ -42,3 +43,21 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+app.get('/python', (req, res) => {
+
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn('python', ['search.py']);
+  // collect data from script
+  python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend)
+  });
+
+})

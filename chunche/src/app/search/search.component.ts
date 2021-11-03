7e2dcabbff9c6f0b1238a5d36aac8921a520d466
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2021-11-02 20:05:23 -0600

    cambio finales

--- chunche/src/app/search/search.component.ts
+++ chunche/src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,20 +8,27 @@ import { Component, OnInit } from '@angular/core';
 
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
 
   }
   files: File[] = [];
-
-  onSelect(event: any) {
+  content:any[] =[]
+  async onSelect(event: any) {
     console.log(event);
     console.log(this.files)
     this.files.push(...event.addedFiles);
+    await this.getData()
+    console.log(await this.content)
   }
 
   onRemove(event: any) {
     console.log(event);
     this.files.splice(this.files.indexOf(event), 1);
+  }
+  async getData(){
+    await this.http.get<any>('localhost:8080/python').subscribe(data => {
+      this.content = data;
+    })
   }
 }
